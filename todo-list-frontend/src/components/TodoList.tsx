f0b14be870859af6195c76ea6ee9dedad171a16c
Francesco Galisi <galisifrancesco@gmail.com>
2023-04-03 17:59:29 +0200

    Implement add todo task

--- todo-list-frontend/src/components/TodoList.tsx
+++ todo-list-frontend/src/components/TodoList.tsx
@@ -11,9 +11,23 @@ export const TodoList: FC = function TodoList() {
 
   const [tasksDone, setTasksDone] = useState<TaskType[]>([]);
 
+  const handleCreateTodoTask = useCallback(() => {
+    setTasksTodo([
+      ...tasksTodo,
+      createTask({
+        name: "A new task",
+        description: "New 'n tasty",
+      }),
+    ]);
+  }, []);
+
   return (
     <div className="flex flex-row justify-between gap-5">
-      <TodoListColumn columnName={"TODO"} tasks={tasksTodo} />
+      <TodoListColumn
+        columnName={"TODO"}
+        tasks={tasksTodo}
+        onCreate={handleCreateTodoTask}
+      />
       <TodoListColumn columnName={"Done"} tasks={tasksDone} />
     </div>
   );
@@ -22,14 +36,23 @@ export const TodoList: FC = function TodoList() {
 interface TodoListColumnProps {
   columnName: string;
   tasks: TaskType[];
+  onCreate?: () => void;
 }
 const TodoListColumn: FC<TodoListColumnProps> = function TodoListColumn({
   columnName,
   tasks,
+  onCreate,
 }) {
   return (
     <div className="w-80 flex flex-col items-center min-h-[30rem] bg-slate-300">
-      <p className="p-2">{columnName}</p>
+      <div className="w-full flex flex-row justify-center items-center gap-4">
+        <p className="p-2">{columnName}</p>
+        {onCreate && (
+          <button className="p-1 rounded bg-slate-50" onClick={onCreate}>
+            add
+          </button>
+        )}
+      </div>
       <div className="w-full flex flex-col gap-4 px-2">
         {tasks.map((task) => (
           <TaskCard key={task.id} task={task} />
@@ -85,7 +108,6 @@ const TaskCard: FC<TaskCardProps> = function TaskCard({ task }) {
           "opacity-0": isBeingDragged,
         }
       )}
-      // className="w-full flex flex-col rounded-3xl p-4 bg-slate-50"
     >
       <h3>{name}</h3>
       <p>{description}</p>
