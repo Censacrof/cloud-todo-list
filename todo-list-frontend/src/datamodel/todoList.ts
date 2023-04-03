8a2528179bdc5a7dcec8b6caa4a54271522518a2
Francesco Galisi <galisifrancesco@gmail.com>
2023-04-04 00:32:02 +0200

    Created datamodel with runtypes

---
+++ todo-list-frontend/src/datamodel/todoList.ts
@@ -0,0 +1,21 @@
+import * as rt from "runtypes";
+
+export const Task = rt.Record({
+  id: rt.String,
+  name: rt.String,
+  description: rt.Number.optional(),
+});
+export type TaskType = rt.Static<typeof Task>;
+
+export const TasksCollection = rt.Record({
+  id: rt.String,
+  name: rt.String,
+  tasks: rt.Array(Task),
+});
+export type TasksCollectionType = rt.Static<typeof TasksCollection>;
+
+export const Board = rt.Record({
+  id: rt.String,
+  collections: rt.Array(TasksCollection),
+});
+export type BoardType = rt.Static<typeof Board>;
